1ff3f9c6bb9dab9f8dcc8661d35be56ba6bfbbc4
Isaiah Norton <isaiah@tiledb.com>
2022-07-29 14:34:56 -0400

    Misc minor openslide convert cleanup

--- util/convert_openslide.py
+++ util/convert_openslide.py
@@ -9,8 +9,6 @@ import openslide as osd
 
 from concurrent.futures import ProcessPoolExecutor
 
-tpath = "/Users/inorton/work/scratch/2022/0711-jnj-path-demo/test4-small/C3L-02964-26.svs"
-#%%
 def create_schema(img_shape):
     img_shape = tuple((img_shape[0], img_shape[1], 3)) # swappity
     print(img_shape)
@@ -33,16 +31,18 @@ def create_schema(img_shape):
     )
     return schema
 
-#s = create_schema(img.level_dimensions[2])
-#print(s)
-
-#%%
 def convert_image(input_img_path, img_group_path, doit=True, level_min=0):
+    """
+    Convert an OpenSlide-supported image to a TileDB Group of Arrays, one
+    per level.
+
+    Usage: convert_image(img_uri, output_uri, doit=True)
+    """
     img = osd.OpenSlide(input_img_path)
 
-    if doit:
-        tiledb.group_create(img_group_path)
+    tiledb.group_create(img_group_path)
 
+    # Build image arrays
     for level in range(img.level_count)[level_min:]:
         dims = img.level_dimensions[level]
 
@@ -59,29 +59,16 @@ def convert_image(input_img_path, img_group_path, doit=True, level_min=0):
             with tiledb.open(output_img_path, "w") as A:
                 A[:] = data
 
-    if doit:
-        # TODO Collect more metadata
-        with tiledb.Group(img_group_path, "w") as G:
-            G.meta["original_filename"] = input_img_path
-            G.meta["level_downsamples"] = img.level_downsamples
+    # Write group metadata
+    with tiledb.Group(img_group_path, "w") as G:
+        G.meta["original_filename"] = input_img_path
+        G.meta["level_downsamples"] = img.level_downsamples
 
-
-
-img_uri = "/Users/inorton/work/scratch/2022/0711-jnj-path-demo/test4-small/C3N-02572-22.svs"
-output_uri = "/Users/inorton/work/scratch/2022/0711-jnj-path-demo/test4-small/convert-test/C3N-02572-22.tdg"
-
-if True and os.path.isdir(output_uri):
-    breakpoint()
-    shutil.rmtree(output_uri)
-
-convert_image(
-    img_uri,
-    output_uri,
-    True
-)
-
-#%%
 def convert_all(path, output_path, level_min=0):
+    """
+    Batch convert a group of .svs files in `path` to TileDB image array groups
+    in `output_path`.
+    """
     paths = glob.glob(f"{path}/*.svs")
 
     print(f"found {len(paths)} .svs files in '{path}'")
@@ -99,25 +86,26 @@ def convert_all(path, output_path, level_min=0):
 
             print(f"output name: {group_path}")
 
-            #convert_image(
-            #    p,
-            #    group_path,
-            #    doit=True,
-            #    level_min=level_min
-            #)
+            if True:
+                TP.submit(
+                    convert_image,
+                    p,
+                    group_path,
+                    doit=True,
+                    level_min=level_min
+                )
+            else:
+                # debugging
+                convert_image(
+                    p,
+                    group_path,
+                    doit=True,
+                    level_min=level_min
+                )
 
-            TP.submit(
-                convert_image,
-                p,
-                group_path,
-                doit=True,
-                level_min=level_min
-            )
 
 # %%
 if __name__ == "__main__":
-    #tmp = tempfile.mkdtemp()
-
     output_path = "/staging/conv1"
 
     convert_all("/staging/orig", output_path, level_min=1)
