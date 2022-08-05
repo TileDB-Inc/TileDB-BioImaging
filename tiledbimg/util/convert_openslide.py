655b860b7c4b8a7a7b4c92acda11f41ae11b2ec4
Kimia Tajik <kimia.tajik@tiledb.com>
2022-08-05 14:53:18 -0400

    Initial comment structure added to convert_openslide.py and conver_tiff.py files
    Changes to convert_openslide.py and util/convert_tiff.py files
    
    changes made to convert_openslide.py
    
    Documentation added
    
    (1) added one new line between description text and the parameters, (2) added the description for parameter level_min
    
    added space between description and code
    
    Implemented test of conversion from a very small OME-TIFF sample file into TileDB pyramidal image group.
    
    Removed .ipynb_checkpoints
    
    Deleted .ipynb_checkpoints
    
    Deleted .DS_Store
    
    Removed .DS_Store
    
    Deleted unused files
    
    Deleted unused files
    
    Deleted unused files
    
    Removed unused code
    
    Removed unused code
    
    Changed data/code directories and updatedtest for ometiff
    
    Removed unsued files
    
    Removed unused files
    
    Removed unused files
    
    Deleted unused files
    
    Removed unused files
    
    Extra lines removed
    
    Removed unused files
    
    Ran black formatter on .py files
    
    Ran black formatter on .py files
    
    Removed unsued files
    
    Implemented OME-Zarr importer and test
    
    Removed unused files
    
    Removed unused files
    
    Removed unused files
    
    Removed unused files
    
    Removed ununsed files
    
    Level info equality reimplemented
    
    Create demo.yml
    
    Create ci.yml
    
    Removed unused files
    
    checked object type before comparison
    
    Update ci.yml
    
    Delete demo.yml
    
    Update ci.yml
    
    Update ci.yml
    
    Update ci.yml
    
    Update ci.yml
    
    Update ci.yml
    
    Update ci.yml
    
    Update ci.yml
    
    Update ci.yml
    
    updated and tested the implementation for level info eqaulity
    
    removed unused files
    
    removed unused files
    
    changed dtype from int32 to uint32
    
    removed unused files
    
    removed unused files
    
    dtype and equality syntax changed
    
    removed unused files
    
    removed unused files

--- tiledbimg/util/convert_openslide.py
+++ tiledbimg/util/convert_openslide.py
@@ -1,4 +1,3 @@
-# Task: Add documentation 
 from attr import attrs
 import tiledb
 import numpy as np
@@ -10,8 +9,19 @@ from concurrent.futures import ProcessPoolExecutor
 
 from common import ImageConverter, create_schema
 
+
 class OpenSlideConverter(ImageConverter):
     def convert(self, input_img_path, img_group_path, level_min=0):
+
+        """
+        Convert a OpenSlide-supported image to a TileDB Group of Arrays, one
+        per level.
+
+        :param input_img_path: path to the OpenSlide-supported image
+        :param img_group_path: path to the TildDB group of arrays
+        :param level_min: minimum level of the image to be converted, by default set to 0 to convert all levels.
+        """
+
         img = osd.OpenSlide(input_img_path)
 
         tiledb.group_create(img_group_path)
@@ -27,9 +37,11 @@ class OpenSlideConverter(ImageConverter):
             schema = create_schema(dims)
             tiledb.Array.create(output_img_path, schema)
 
-            slide_data = img.read_region((0,0), level, dims).convert("RGB")
-            data = np.array(slide_data).swapaxes(0,1)
-            newdata = data.view(dtype=np.dtype([("", 'uint8'), ("", 'uint8'), ("", 'uint8')]))
+            slide_data = img.read_region((0, 0), level, dims).convert("RGB")
+            data = np.array(slide_data).swapaxes(0, 1)
+            newdata = data.view(
+                dtype=np.dtype([("", "uint8"), ("", "uint8"), ("", "uint8")])
+            )
             with tiledb.open(output_img_path, "w") as A:
                 A[:] = newdata
 
@@ -38,22 +50,27 @@ class OpenSlideConverter(ImageConverter):
             G.meta["original_filename"] = input_img_path
             G.meta["level_downsamples"] = img.level_downsamples
 
+
 def convert_image(input_img_path, img_group_path, doit=True, level_min=0):
+
     """
     Convert an OpenSlide-supported image to a TileDB Group of Arrays, one
     per level.
 
     Usage: convert_image(img_uri, output_uri, doit=True)
     """
+
     converter = ImageConverter()
     converter.convert(input_img_path, img_group_path)
 
 
 def convert_all(path, output_path, level_min=0):
+
     """
     Batch convert a group of .svs files in `path` to TileDB image array groups
     in `output_path`.
     """
+
     paths = glob.glob(f"{path}/*.svs")
 
     print(f"found {len(paths)} .svs files in '{path}'")
@@ -72,21 +89,10 @@ def convert_all(path, output_path, level_min=0):
             print(f"output name: {group_path}")
 
             if True:
-                TP.submit(
-                    convert_image,
-                    p,
-                    group_path,
-                    doit=True,
-                    level_min=level_min
-                )
+                TP.submit(convert_image, p, group_path, doit=True, level_min=level_min)
             else:
                 # debugging
-                convert_image(
-                    p,
-                    group_path,
-                    doit=True,
-                    level_min=level_min
-                )
+                convert_image(p, group_path, doit=True, level_min=level_min)
 
 
 # %%
