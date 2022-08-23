7038c90e3eb2a2e343d42dbc96eda36487977c5c
Kimia Tajik <kimia.tajik@tiledb.com>
2022-08-23 13:03:56 -0400

    Removed unused code

--- tiledbimg/test_ome_tiff.py
+++ tiledbimg/test_ome_tiff.py
@@ -21,19 +21,19 @@ def test_ome_tiff():
 
 #ToDo: We need to find better test data. This data has already been downsampled without preserving the original levels.
 
-#    assert(
-#         t.level_count == 3
-#     )
+   assert(
+        t.level_count == 3
+    )
 
-#    assert(
-#         t.dimensions == (2220, 2967)
-#     )
-#    assert(
-#         t.level_dimensions == ((2220, 2967), (387, 463), (1280, 431))
-#     )
-#    assert(
-#         t.level_downsamples == None
-#     )
+   assert(
+        t.dimensions == (2220, 2967)
+    )
+   assert(
+        t.level_dimensions == ((2220, 2967), (387, 463), (1280, 431))
+    )
+   assert(
+        t.level_downsamples == None
+    )
 
    assert(
         t.level_info == [LevelInfo(level=0, shape=(2220, 2967)), LevelInfo(level=1, shape=(387, 463)), LevelInfo(level=2, shape=(1280, 431))]
