262fb90e8fd6433275f3bb9002096e638981e428
Kimia Tajik <kimia.tajik@tiledb.com>
2022-08-23 22:04:14 -0400

    Ran black formatter on .py files

--- tiledbimg/util/common.py
+++ tiledbimg/util/common.py
@@ -4,10 +4,13 @@ import numpy as np
 from dataclasses import dataclass
 import os
 
+
 @dataclass
 class LevelTile:
     x: int
     y: int
+
+
 class ImageConverter(object):
     input_image: str
     output_group: str
@@ -21,32 +24,40 @@ class ImageConverter(object):
 
     def level_tile(self, shape):
         level_tile = LevelTile(
-            np.min((self.tile_x, shape[0])),
-            np.min((self.tile_y, shape[1]))
+            np.min((self.tile_x, shape[0])), np.min((self.tile_y, shape[1]))
         )
         return level_tile
 
     def create_schema(self, img_shape):
-        img_shape = tuple((img_shape[0], img_shape[1], 3)) # swappity
+        img_shape = tuple((img_shape[0], img_shape[1], 3))  # swappity
         print("Processing level with shape: ", img_shape)
         dims = []
 
         level_tile = self.level_tile(img_shape)
 
         dims.append(
-            tiledb.Dim(name="X", domain=(0,img_shape[0]-1), dtype=np.uint64, tile=level_tile.x)
+            tiledb.Dim(
+                name="X",
+                domain=(0, img_shape[0] - 1),
+                dtype=np.uint64,
+                tile=level_tile.x,
+            )
         )
         dims.append(
-                tiledb.Dim(name="Y", domain=(0,img_shape[1]-1), dtype=np.uint64, tile=level_tile.y)
+            tiledb.Dim(
+                name="Y",
+                domain=(0, img_shape[1] - 1),
+                dtype=np.uint64,
+                tile=level_tile.y,
+            )
         )
 
         filters = [tiledb.ZstdFilter(level=0)]
         attr = tiledb.Attr(
-            name='rgb', dtype=[("", 'uint8'), ("", 'uint8'), ("", 'uint8')], filters=filters
+            name="rgb",
+            dtype=[("", "uint8"), ("", "uint8"), ("", "uint8")],
+            filters=filters,
         )
 
-        schema = tiledb.ArraySchema(
-            tiledb.Domain(*dims),
-            attrs=[attr]
-        )
+        schema = tiledb.ArraySchema(tiledb.Domain(*dims), attrs=[attr])
         return schema
