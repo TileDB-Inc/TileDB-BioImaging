f6c172418453044f88a02e1dfac56d8bcb7e0c22
George Sakkis <george.sakkis@tiledb.com>
2022-11-30 18:18:24 +0200

    Add ImageReader.{level_dtype,level_shape} abstract methods

--- tiledb/bioimg/converters/ome_tiff.py
+++ tiledb/bioimg/converters/ome_tiff.py
@@ -1,4 +1,4 @@
-from typing import Any, Dict, Mapping
+from typing import Any, Dict, Mapping, Tuple, cast
 
 import jsonpickle as json
 import numpy as np
@@ -29,8 +29,14 @@ class OMETiffReader(ImageReader):
     def level_count(self) -> int:
         return len(self._series.levels)
 
+    def level_dtype(self, level: int) -> np.dtype:
+        return self._series.levels[level].dtype
+
+    def level_shape(self, level: int) -> Tuple[int, ...]:
+        return cast(Tuple[int, ...], self._series.levels[level].shape)
+
     def level_image(self, level: int) -> np.ndarray:
-        return self._series.asarray(level=level)
+        return self._series.levels[level].asarray()
 
     def level_metadata(self, level: int) -> Dict[str, Any]:
         if level == 0:
