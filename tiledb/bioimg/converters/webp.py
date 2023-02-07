732127967c4efb604cea983ca5bb34cd8af36db2
George Sakkis <george.sakkis@tiledb.com>
2023-02-07 03:02:55 +0200

    Drop FromWebPAxesMapper

--- tiledb/bioimg/converters/webp.py
+++ tiledb/bioimg/converters/webp.py
@@ -1,4 +1,4 @@
-from .axes import YX_TO_YXC, YXC_TO_YX, Axes, AxesMapper
+from .axes import YXC_TO_YX, Axes, AxesMapper
 
 
 class ToWebPAxesMapper(AxesMapper):
@@ -7,11 +7,3 @@ class ToWebPAxesMapper(AxesMapper):
     def __init__(self, source: Axes, c_size: int):
         super().__init__(source, target=Axes("YXC"))
         self._transforms.append(YXC_TO_YX(c_size))
-
-
-class FromWebPAxesMapper(AxesMapper):
-    """Mapper from 2D axes (YX) to 3D (YXC or permutations thereof)"""
-
-    def __init__(self, target: Axes, c_size: int):
-        super().__init__(Axes("YXC"), target)
-        self._transforms.insert(0, YX_TO_YXC(c_size))
