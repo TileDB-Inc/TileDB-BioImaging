Added dimension metadata, fixed webp scaling

tiledb/bioimg/converters/base.py
@@ -5,6 +5,7 @@ import warnings
 from abc import ABC, abstractmethod
 from concurrent.futures import ThreadPoolExecutor
 from operator import itemgetter
+from threading import Lock
 from typing import Any, Dict, Mapping, Optional, Sequence, Tuple, Type, Union
 
 import numpy as np
@@ -267,19 +268,48 @@ class ImageConverter:
                     max_workers=max_workers,
                     compressor=compressor,
                 )
+
+                dimensions = []
+                shape_mapper = reader.axes.mapper(
+                    reader.axes
+                    if preserve_axes
+                    else reader.axes.canonical(reader.level_shape(level_min))
+                )
+
                 if pyramid_kwargs is not None:
                     if level_min < reader.level_count - 1:
                         warnings.warn(
                             f"The image contains multiple levels but only level {level_min} "
                             "will be considered for generating the image pyramid"
                         )
-                    uri = _convert_level_to_tiledb(level_min, **convert_kwargs)
-                    create_image_pyramid(
-                        rw_group, uri, level_min, max_tiles, compressor, pyramid_kwargs
+                    uri, min_max_channels = _convert_level_to_tiledb(
+                        level_min, **convert_kwargs
+                    )
+
+                    dimensions.append(reader.level_shape(level_min))
+
+                    dimensions += create_image_pyramid(
+                        rw_group,
+                        uri,
+                        level_min,
+                        max_tiles,
+                        compressor,
+                        pyramid_kwargs,
+                        reader.level_shape(level_min),
+                        preserve_axes,
+                        reader.axes,
+                        reader.level_dtype(level_min),
                     )
                 else:
                     for level in range(level_min, reader.level_count):
-                        _convert_level_to_tiledb(level, **convert_kwargs)
+                        dimensions.append(reader.level_shape(level))
+
+                        if level == level_min:
+                            _, min_max_channels = _convert_level_to_tiledb(
+                                level, **convert_kwargs
+                            )
+                        else:
+                            _convert_level_to_tiledb(level, **convert_kwargs)
 
             metadata = reader.image_metadata
             metadata["axes_mapping"] = get_axes_mapping(
@@ -288,6 +318,13 @@ class ImageConverter:
                 if preserve_axes
                 else reader.axes.canonical(reader.level_shape(level_min)).dims,
             )
+            metadata["dimensions"] = [
+                shape_mapper.map_shape(shape) for shape in dimensions
+            ]
+
+            for idx in range(len(metadata["Channels"])):
+                metadata["Channels"][idx]["Min"] = min_max_channels[idx][0]
+                metadata["Channels"][idx]["Max"] = min_max_channels[idx][1]
 
             with rw_group:
                 rw_group.w_group.meta.update(
@@ -317,7 +354,7 @@ def _convert_level_to_tiledb(
     chunked: bool,
     max_workers: int,
     compressor: tiledb.Filter,
-) -> str:
+) -> Tuple[str, Sequence[Sequence[int | float]]]:
     # create mapper from source to target axes
     source_axes = reader.axes
     source_shape = reader.level_shape(level)
@@ -339,24 +376,55 @@ def _convert_level_to_tiledb(
     attr_dtype = reader.level_dtype(level)
     schema = get_schema(dim_names, dim_shape, max_tiles, attr_dtype, compressor)
 
+    channel_index = source_axes.dims.find("C")
+    channel_count = source_shape[channel_index]
+    channel_min_max = [
+        [
+            np.iinfo(reader.level_dtype(level)).max,
+            np.iinfo(reader.level_dtype(level)).min,
+        ]
+        for _ in range(channel_count)
+    ]
+
     # get or create TileDB array uri
     uri, created = rw_group.get_or_create(f"l_{level}.tdb", schema)
     if created:
         # write image and metadata to TileDB array
         with open_bioimg(uri, "w") as out_array:
             out_array.meta.update(reader.level_metadata(level), level=level)
+            min_max_axes = tuple(
+                idx for idx in range(len(source_axes.dims)) if idx != channel_index
+            )
+
             if chunked or max_workers:
                 inv_axes_mapper = axes_mapper.inverse
+                min_max_lock = Lock()
 
-                def tile_to_tiledb(level_tile: Tuple[slice, ...]) -> None:
+                def tile_to_tiledb(level_tile: Tuple[slice, ...], lock: Lock) -> None:
                     source_tile = inv_axes_mapper.map_tile(level_tile)
                     image = reader.level_image(level, source_tile)
                     out_array[level_tile] = axes_mapper.map_array(image)
 
+                    minimum = np.amin(image, axis=min_max_axes)
+                    maximum = np.amax(image, axis=min_max_axes)
+
+                    with lock:
+                        for channel in range(channel_count):
+                            channel_min_max[channel][0] = min(
+                                channel_min_max[channel][0], minimum.item(channel)
+                            )
+                            channel_min_max[channel][1] = max(
+                                channel_min_max[channel][1], maximum.item(channel)
+                            )
+
                 ex = ThreadPoolExecutor(max_workers) if max_workers else None
                 mapper = getattr(ex, "map", map)
                 for _ in tqdm(
-                    mapper(tile_to_tiledb, iter_tiles(out_array.domain)),
+                    mapper(
+                        tile_to_tiledb,
+                        iter_tiles(out_array.domain),
+                        [min_max_lock] * num_tiles(out_array.domain),
+                    ),
                     desc=f"Ingesting level {level}",
                     total=num_tiles(out_array.domain),
                     unit="tiles",
@@ -367,4 +435,16 @@ def _convert_level_to_tiledb(
             else:
                 image = reader.level_image(level)
                 out_array[:] = axes_mapper.map_array(image)
-    return uri
+
+                minimum = np.amin(image, axis=min_max_axes)
+                maximum = np.amax(image, axis=min_max_axes)
+
+                for channel in range(channel_count):
+                    channel_min_max[channel][0] = min(
+                        channel_min_max[channel][0], minimum.item(channel)
+                    )
+                    channel_min_max[channel][1] = max(
+                        channel_min_max[channel][1], maximum.item(channel)
+                    )
+
+    return uri, channel_min_max

tiledb/bioimg/converters/ome_zarr.py
@@ -19,26 +19,34 @@ from .base import ImageConverter, ImageReader, ImageWriter
 class OMEZarrReader(ImageReader):
     @property
     def image_metadata(self) -> Dict[str, Any]:
-        metadata = {}
+        metadata: Dict[str, Any] = {}
         node_metadata = self._multiscales.node.metadata
-
         color_generator = iter_color(self.level_dtype(0))
         channels = []
         for idx in range(len(node_metadata.get("name"))):
-            channel = {"ID": f"{idx}", "Name": node_metadata.get("name")[idx]}
+            channel: Dict[str, Any] = {
+                "ID": f"{idx}",
+                "Name": node_metadata.get("name")[idx],
+            }
 
             if "colormap" in node_metadata:
                 channel["Color"] = {
-                    "red": node_metadata.get("colormap")[idx][1][0]
-                    * np.iinfo(self.level_dtype(0)).max,
-                    "green": node_metadata.get("colormap")[idx][1][1]
-                    * np.iinfo(self.level_dtype(0)).max,
-                    "blue": node_metadata.get("colormap")[idx][1][2]
-                    * np.iinfo(self.level_dtype(0)).max,
-                    "alpha": np.iinfo(self.level_dtype(0)).max,
+                    "red": int(
+                        node_metadata.get("colormap")[idx][1][0]
+                        * np.iinfo(self.level_dtype(0)).max
+                    ),
+                    "green": int(
+                        node_metadata.get("colormap")[idx][1][1]
+                        * np.iinfo(self.level_dtype(0)).max
+                    ),
+                    "blue": int(
+                        node_metadata.get("colormap")[idx][1][2]
+                        * np.iinfo(self.level_dtype(0)).max
+                    ),
+                    "alpha": int(np.iinfo(self.level_dtype(0)).max),
                 }
             else:
-                channel["Color"]: next(color_generator)
+                channel["Color"] = next(color_generator)
 
             if "contrast_limits" in node_metadata:
                 channel["Min"] = node_metadata["contrast_limits"][idx][0]
@@ -52,7 +60,7 @@ class OMEZarrReader(ImageReader):
 
     @property
     def original_metadata(self) -> Dict[str, Any]:
-        metadata = {"ZARR": {}}
+        metadata: Dict[str, Any] = {"ZARR": {}}
 
         metadata["ZARR"]["MULTISCALE"] = self._multiscales
         metadata["ZARR"]["OMERO"] = self._omero

tiledb/bioimg/helpers.py
@@ -2,7 +2,7 @@ from __future__ import annotations
 
 import os
 from pathlib import Path
-from typing import Any, Iterator, Mapping, Sequence, Tuple
+from typing import Any, Dict, Iterator, Mapping, MutableMapping, Sequence, Tuple
 from urllib.parse import urlparse
 
 import numpy as np
@@ -104,20 +104,37 @@ def create_image_pyramid(
     rw_group: ReadWriteGroup,
     base_uri: str,
     base_level: int,
-    max_tiles: Mapping[str, int],
+    max_tiles: MutableMapping[str, int],
     compressor: tiledb.Filter,
     pyramid_kwargs: Mapping[str, Any],
-) -> None:
-    with open_bioimg(base_uri) as a:
-        base_shape = a.shape
-        dim_names = tuple(dim.name for dim in a.domain)
-        dim_axes = "".join(dim_names)
-        attr_dtype = a.attr(0).dtype
+    base_shape: Tuple[int, ...],
+    preserve_axes: bool,
+    base_axes: Axes,
+    attr_dtype: np.dtype,
+) -> Sequence[Tuple[int, ...]]:
+    pixel_depth = get_pixel_depth(compressor)
+    if pixel_depth == 1:
+        if preserve_axes:
+            target_axes = base_axes
+        else:
+            target_axes = base_axes.canonical(base_shape)
+        axes_mapper = base_axes.mapper(target_axes)
+        dim_names = tuple(target_axes.dims)
+    else:
+        max_tiles["X"] *= pixel_depth
+        axes_mapper = base_axes.webp_mapper(pixel_depth)
+        dim_names = ("Y", "X")
 
-    scaler = Scaler(base_shape, dim_axes, **pyramid_kwargs)
+    scaler = Scaler(base_shape, base_axes.dims, **pyramid_kwargs)
     for i, dim_shape in enumerate(scaler.level_shapes):
         level = base_level + 1 + i
-        schema = get_schema(dim_names, dim_shape, max_tiles, attr_dtype, compressor)
+        schema = get_schema(
+            dim_names,
+            axes_mapper.map_shape(dim_shape),
+            max_tiles,
+            attr_dtype,
+            compressor,
+        )
         uri, created = rw_group.get_or_create(f"l_{level}.tdb", schema)
         if not created:
             continue
@@ -125,12 +142,13 @@ def create_image_pyramid(
         with open_bioimg(uri, mode="w") as out_array:
             out_array.meta.update(level=level)
             with open_bioimg(base_uri) as in_array:
-                scaler.apply(in_array, out_array, i)
+                scaler.apply(in_array, out_array, i, axes_mapper)
 
         # if a non-progressive method is used, the input layer of the scaler
         # is the base image layer else we use the previously generated layer
         if scaler.progressive:
             base_uri = uri
+    return scaler.level_shapes
 
 
 def iter_levels_meta(group: tiledb.Group) -> Iterator[Mapping[str, Any]]:
@@ -173,7 +191,7 @@ def get_axes_mapping(
     return {axis: [axis] for axis in axes}
 
 
-def iter_color(attr_type: np.dtype) -> Iterator[Mapping[str, Any]]:
+def iter_color(attr_type: np.dtype) -> Iterator[Dict[str, int]]:
     yield {
         "red": np.iinfo(attr_type).max,
         "green": np.iinfo(attr_type).min,
@@ -241,4 +259,5 @@ def get_rgba(value: int) -> dict[str, int]:
 
 
 def length_converter(value: float, original_unit: str, requested_unit: str) -> float:
-    return value * 10 ** (LENGTH_UNITS[original_unit] - LENGTH_UNITS[requested_unit])
+    result = value * 10 ** (LENGTH_UNITS[original_unit] - LENGTH_UNITS[requested_unit])
+    return float(result)