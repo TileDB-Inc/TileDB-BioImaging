3cde9f877342c51ebffec6dce510c365fb48ec02
Isaiah Norton <ihnorton@users.noreply.github.com>
2023-06-02 17:04:45 -0400

    Update __init__.py

--- tiledb/bioimg/__init__.py
+++ tiledb/bioimg/__init__.py
@@ -1,6 +1,6 @@
-from .types import Converters
-from .wrappers import from_bioimg
-
 ATTR_NAME = "intensity"
 WHITE_RGB = 16777215  # FFFFFF in hex
 WHITE_RGBA = 4294967295  # FFFFFFFF in hex
+
+from .types import Converters
+from .wrappers import from_bioimg
