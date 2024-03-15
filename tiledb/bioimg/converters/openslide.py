a14cdc226f55d4a945c5c7940ca8c4fee2f9dd19
ktsitsi <kostas.t@tiledb.com>
2024-03-15 18:32:13 +0200

    Precommit fix

--- tiledb/bioimg/converters/openslide.py
+++ tiledb/bioimg/converters/openslide.py
@@ -1,10 +1,12 @@
 import logging
+import os
 from typing import Any, Dict, Optional, Sequence, Tuple, cast
-from . import WIN_OPENSLIDE_PATH
+
 import numpy as np
 
-import os
-if hasattr(os, 'add_dll_directory'):
+from . import WIN_OPENSLIDE_PATH
+
+if hasattr(os, "add_dll_directory"):
     # Python >= 3.8 on Windows
     with os.add_dll_directory(WIN_OPENSLIDE_PATH):
         import openslide as osd
