839477c366c8a56dc3ce696834de27e0b785c0fd
Konstantinos Tsitsimpikos <ktsitsi@users.noreply.github.com>
2024-07-03 14:42:14 +0300

    Change Openslide Drive Path for Windows (#131)
    * Testing C dir
    
    * Update Readme installation directions for openslide
    
    * Define output for coverage

--- tiledb/bioimg/converters/__init__.py
+++ tiledb/bioimg/converters/__init__.py
@@ -2,4 +2,4 @@ FMT_VERSION = 2
 DATASET_TYPE = "bioimg"
 DEFAULT_SCRATCH_SPACE = "/dev/shm"
 # Windows use only
-WIN_OPENSLIDE_PATH = r"D:\openslide-win64\bin"
+WIN_OPENSLIDE_PATH = r"C:\openslide-win64\bin"
