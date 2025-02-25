6bf3dec5fb4bf6a68f78103329da2bbc71cf3d10
Konstantinos Tsitsimpikos <ktsitsi@users.noreply.github.com>
2025-02-25 11:39:15 -0500

    Bioimg package dependencies (#153)
    * Testing automatic openslide-bin installation for all platforms
    
    * Remove unnecessary os conditions and readme
    
    * Remove unnecessary CI steps

--- tests/integration/converters/__init__.py
+++ tests/integration/converters/__init__.py
@@ -1,6 +1,1 @@
-import os
-from tiledb.bioimg import WIN_OPENSLIDE_PATH
 
-if hasattr(os, "add_dll_directory"):
-    # Python >= 3.8 on Windows
-    os.add_dll_directory(WIN_OPENSLIDE_PATH)
--- tiledb/bioimg/__init__.py
+++ tiledb/bioimg/__init__.py
@@ -2,39 +2,22 @@ ATTR_NAME = "intensity"
 WHITE_RGB = 16777215  # FFFFFF in hex
 WHITE_RGBA = 4294967295  # FFFFFFFF in hex
 EXPORT_TILE_SIZE = 256
-WIN_OPENSLIDE_PATH = r"C:\openslide-win64\bin"
 
 import importlib.util
-import os
 import warnings
 from typing import Optional
 from .types import Converters
 
 _osd_exc: Optional[ImportError]
-
-if hasattr(os, "add_dll_directory"):
-    # Python >= 3.8 on Windows
-    with os.add_dll_directory(WIN_OPENSLIDE_PATH):
-        try:
-            importlib.util.find_spec("openslide")
-        except ImportError as err_osd:
-            warnings.warn(
-                "Openslide Converter requires 'openslide-python' package. "
-                "You can install 'tiledb-bioimg' with the 'openslide' or 'full' flag"
-            )
-            _osd_exc = err_osd
-        else:
-            _osd_exc = None
+try:
+    importlib.util.find_spec("openslide")
+except ImportError as err_osd:
+    warnings.warn(
+        "Openslide Converter requires 'openslide-python' package. "
+        "You can install 'tiledb-bioimg' with the 'openslide' or 'full' flag"
+    )
+    _osd_exc = err_osd
 else:
-    try:
-        importlib.util.find_spec("openslide")
-    except ImportError as err_osd:
-        warnings.warn(
-            "Openslide Converter requires 'openslide-python' package. "
-            "You can install 'tiledb-bioimg' with the 'openslide' or 'full' flag"
-        )
-        _osd_exc = err_osd
-    else:
-        _osd_exc = None
+    _osd_exc = None
 
 from .wrappers import *
